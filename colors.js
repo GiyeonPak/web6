6cb01a40d6792d0ec78d5d64daac759c2526ac92
GiyeonPak <40662825+GiyeonPak@users.noreply.github.com>
2018-07-30 15:03:28 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,38 @@
+var body = {
+  setColor:function(color){
+    document.querySelector('body').style.color=color;
+  },
+  setBackgroundColor:function(color){
+    document.querySelector('body').style.backgroundColor=color;
+  },
+  listSetColor:function(color){
+  //   var alist = document.querySelectorAll('a');
+  //   var i = 0;
+  //   while(i<alist.length)
+  //   {
+  //     alist[i].style.color=color;
+  //     i = i + 1;
+  //   }
+  // }
+  $('a').css('color',color);
+}
+}
+
+function night_day_handler(self)
+{
+  var target = document.querySelector('body');
+  if(self.value === 'night')
+  {
+    body.setBackgroundColor('black');
+    body.setColor('white');
+    self.value='day';
+    body.listSetColor('powderblue');
+  }
+  else{
+    body.setBackgroundColor('white');
+    body.setColor('black');
+    self.value='night';
+    body.listSetColor('blue');
+  }
+
+}
